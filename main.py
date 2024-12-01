7d3905462598eaa2bb60cfb068406e9b6868ee69
Joan Jara Bosch <jaraboschj@gmail.com>
2024-12-01 21:42:03 +0100

    Day 1
    Puzzle day1 resolved both parts

---
+++ main.py
@@ -0,0 +1,7 @@
+import puzzle_day1
+
+if __name__ == '__main__':
+    print("DAY 1 FIRST PART")
+    print(puzzle_day1.distances())
+    print("DAY 1 SECOND PART")
+    print(puzzle_day1.similarity())
---
+++ puzzle_day1.py
@@ -0,0 +1,31 @@
+def get_data():
+    f = open("input/day1.txt", "r")
+    first_list = []
+    second_list = []
+    for x in f:
+        first_list.append(x.split("   ")[0])
+        second_list.append(x.split("   ")[1].replace("\n", ""))
+
+    first_list.sort()
+    second_list.sort()
+
+    return first_list, second_list
+
+
+def distances():
+    x, y = get_data()
+    distance = 0
+    i = 0
+    while i < len(x):
+        distance = distance + abs(int(x[i]) - int(y[i]))
+        i = i + 1
+    return distance
+
+def similarity():
+    x, y = get_data()
+    similar = 0
+    i = 0
+    while i < len(x):
+        similar = similar + int(x[i]) * y.count(x[i])
+        i = i + 1
+    return similar
