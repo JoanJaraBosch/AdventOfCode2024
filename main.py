97ac53a4fdf553c63ddbb48784455d2e10b82637
Joan Jara Bosch <jaraboschj@gmail.com>
2024-12-02 21:07:42 +0100

    Puzzle day2 solved both parts

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import puzzle_day1
+import puzzle_day2
 
 if __name__ == '__main__':
-    print("DAY 1 FIRST PART")
-    print(puzzle_day1.distances())
-    print("DAY 1 SECOND PART")
-    print(puzzle_day1.similarity())
+
+    x = input("Which puzzle do you want to solve?: ")
+    if int(x) == 1:
+        print("DAY 1 FIRST PART")
+        print(puzzle_day1.distances())
+        print("DAY 1 SECOND PART")
+        print(puzzle_day1.similarity())
+    elif int(x) == 2:
+        print("DAY 2 FIRST PART")
+        puzzle_day2.count_reports_safe()
+        print("DAY 2 SECOND PART")
+        puzzle_day2.count_reports_safe_part2()
---
+++ puzzle_day2.py
@@ -0,0 +1,62 @@
+def check_report(report):
+    cont = 1
+    found = 0
+    first_dif = 0
+    val_anterior = int(report[0])
+    safe = 0
+    while cont < len(report) and found == 0:
+        level = int(report[cont])
+        if cont == 1:
+            first_dif = val_anterior - level
+
+        if cont != 0:
+            if (val_anterior - level) > 3 or (val_anterior - level) < -3 or (val_anterior - level) == 0:
+                found = 1
+            else:
+                if first_dif < 0 < (val_anterior - level):
+                    found = 1
+                elif first_dif > 0 > (val_anterior - level):
+                    found = 1
+        val_anterior = level
+        cont = cont + 1
+    if found == 0:
+        safe = safe + 1
+    return safe
+
+
+def count_reports_safe():
+    reports = open("input/day2.txt", "r")
+    safe = 0
+    for report in reports:
+        report = report.replace("\n", "").split(" ")
+        safe = safe + check_report(report)
+
+    print("There are ", safe, "reports safe")
+
+
+def count_reports_safe_part2():
+    reports = open("input/day2.txt", "r")
+    result = 0
+    for report in reports:
+        cont = 0
+        safe = 0
+        found = 0
+        report = report.replace("\n", "").split(" ")
+        while cont < len(report) and found == 0:
+            if cont == 0:
+                safe = check_report(report)
+                if safe == 0:
+                    report_level_removed = report[cont+1:]
+                    safe = check_report(report_level_removed)
+            elif cont == len(report)-1:
+                report_level_removed = report[:-1]
+                safe = check_report(report_level_removed)
+            else:
+                report_level_removed = report[:cont]+report[cont+1:]
+                safe = check_report(report_level_removed)
+
+            if safe == 1:
+                found = 1
+            cont = cont + 1
+        result = result + safe
+    print("There are ", result, "reports safe")
